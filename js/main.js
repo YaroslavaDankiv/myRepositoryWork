e50cf2683f8f31f820dbc18ac57dbaea09b0ac50
Yaroslava <yaroslavawest@ukr.net>
2017-12-04 20:37:57 +0200

    Added folder js and file

---
+++ js/main.js
@@ -0,0 +1,19 @@
+////////////////////////////////////////
+(function(){
+	var daleyM = 3000;
+	deleteListItem('foreground');
+
+function deleteListItem(cssSelector){
+	var ul = document.querySelector("ul." + cssSelector);
+	var items = ul.getElementsByTagName('li');
+
+	var intervalId =  setInterval(function deleteListItem(){
+			 if(items.length != 0){
+		ul.removeChild(items[0]);
+	}else {
+		clearInterval(intervalId);
+	}
+}, daleyM);
+
+}
+}());
